7e785dff8594256ceea4b25b83c719aecb572052
Xcoderx <xcoder304@gmail.com>
2022-05-12 18:57:27 +0500

    added the contact and about us page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Main from "../components/Main/Main";
-import { selectItems } from "../Redux/features/AllGlobalStates";
-import { useSelector } from "react-redux";
 
 const Home = () => {
-  const items = useSelector(selectItems);
-
-  console.log("this is items", items);
-
   return (
     <div className="app">
       <Main />
