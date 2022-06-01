08fc35262145d489e02fac10a81a457c9a678c9c
Xcoderx <xcoder304@gmail.com>
2022-06-02 01:59:03 +0500

    convert the pincode object to json data

--- pages/api/pincode.js
+++ pages/api/pincode.js
@@ -1,15 +1,4 @@
+import pincodeData from "../jsondata/pincode.json";
 export default function handler(req, res) {
-  res.status(200).json({
-    12200: ["ABBAS PUR"],
-    80820: ["ABDUL GHAFOOR LEHRI"],
-    16320: ["ACHORI"],
-    72200: ["BADIN"],
-    17000: ["Pakka Qilla, Hyderabad"],
-    71800: ["Latifabad Unit 2,Hyderabad"],
-    12345: ["Zamanabad,Karachi"],
-    74000: ["PECHS, Karachi"],
-    53480: ["Bata Pur, Lahore"],
-    40050: ["Nasirabad Aitchison Society, Lahore"],
-    22222: ["G-10, Islamabad"],
-  });
+  res.status(200).json(pincodeData);
 }
