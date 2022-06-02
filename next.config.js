19f6b279f9c29783fac6afd57114abc77a8a0c15
Xcoderx <xcoder304@gmail.com>
2022-06-02 21:55:15 +0500

    v0 is fully compeleted

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   env: {
     HOSTING_NAME: process.env.HOSTING_NAME,
   },
-};
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 module.exports = nextConfig;
